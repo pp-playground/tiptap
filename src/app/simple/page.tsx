abdb3427e3e07c9ebc2578f946f180b6ca5c35f0
pocopota <pocopota.program@gmail.com>
2025-06-11 11:10:38 +0900

    tiptap template

---
+++ src/app/simple/page.tsx
@@ -0,0 +1,8 @@
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
+import '../../styles/_variables.scss';
+import '../../styles/_keyframe-animations.scss';
+
+
+export default function Page() {
+  return <SimpleEditor />
+}
